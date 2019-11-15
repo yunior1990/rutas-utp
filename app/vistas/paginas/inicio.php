e697cee277a7bd5da4ec1bed069bb5a5523bf7c6
yunior <yunioraquise@gmail.com>
2019-11-15 18:01:13 -0500

    cuarto sprint

--- app/vistas/paginas/inicio.php
+++ app/vistas/paginas/inicio.php
@@ -2,29 +2,7 @@
     require ruta_app.'/vistas/inc/header.php';  
     
 ?>
-<?php 
-print('
-	<form class="item" method="post">
-		<div class="p_25">
-			<input type="text" name="idEmpleado" placeholder="usuario" required>
-		</div>
-		<div class="p_25">
-			<input type="password" name="Contraseña" placeholder="password" required>
-		</div>
-		<div class="p_25">
-			<input type="submit" class="button" value="Enviar">
-		</div>
-	</form>
-');
-if( isset($_GET['error']) ) {
-	$template = '
-		<div class="container">
-			<p class="item  error">%s</p>
-		</div>
-	';
-	printf($template, $_GET['error']);
-}
-?>
+<P>SISTEMAS DE RUTAS EN TIEMPO REAL</P>
 <?php   
     require ruta_app.'/vistas/inc/footer.php';
     
